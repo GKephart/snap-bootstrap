1a187c3eec1d3c21a016cab2e97e94abd3f98915
GKephart <gkephart@cnm.edu>
2018-08-06 10:48:01 -0600

    added an icon to the nav and changed the nav color

--- index.php
+++ index.php
@@ -19,8 +19,8 @@
 	</head>
 
 	<body>
-		<nav class="navbar navbar-expand-lg navbar-light bg-dark">
-			<a class="navbar-brand" href="#">Navbar</a>
+		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+			<a class="navbar-brand" href="#"><img src="./images/flaticon.png" alt="thanks flaticon.com/"></a>
 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
