981dd9746f4dfd0bca4c2edb9c1864d8f8ee2596
GKephart <gkephart@cnm.edu>
2018-08-06 18:43:42 -0600

    Added the cards for the bootstrap challenge and updated the nav

--- index.php
+++ index.php
@@ -6,22 +6,30 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1">
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css"
+				integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
 		<!-- Custom CSS -->
 		<link rel="stylesheet" href="css/style.css" type="text/css">
 
 		<!-- javascript necessary for bootstrap components -->
-		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+				  integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+				  crossorigin="anonymous"></script>
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"
+				  integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49"
+				  crossorigin="anonymous"></script>
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"
+				  integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy"
+				  crossorigin="anonymous"></script>
 
 	</head>
 
 	<body>
 		<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
 			<a class="navbar-brand" href="#"><img src="./images/flaticon.png" alt="thanks flaticon.com/"></a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+					  aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
 				<span class="navbar-toggler-icon"></span>
 			</button>
 
@@ -34,7 +42,8 @@
 						<a class="nav-link" href="#">Link</a>
 					</li>
 					<li class="nav-item dropdown">
-						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+						<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown"
+							aria-haspopup="true" aria-expanded="false">
 							Dropdown
 						</a>
 						<div class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -47,12 +56,53 @@
 				</ul>
 			</div>
 		</nav>
-	<br>
-		<div class="container-fluid">
-			<div class="row">
+		<br>
+		<div class="py-2">
+			<div class="container-fluid">
 
+				<div class="card-deck">
+
+					<div class="card border-light mb-5	">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+								card's content.</p>
+						</div>
+					</div>
+
+					<div class="card border-primary mb-5">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+								card's content.</p>
+						</div>
+					</div>
+
+					<div class="card border-danger mb-5">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+								card's content.</p>
+						</div>
+					</div>
+
+					<div class="card border-warning mb-5">
+						<div class="card-header">Header</div>
+						<div class="card-body">
+							<h5 class="card-title">Light card title</h5>
+							<p class="card-text">Some quick example text to build on the card title and make up the bulk of the
+								card's content.</p>
+						</div>
+					</div>
+
+				</div>
 			</div>
-		</div>
+
+
+
 
 	</body>
 </html>
